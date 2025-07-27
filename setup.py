9ed55a622afdd0fe2f7d3a18e5002df2d57efe2f
jerrome2685 <jerrome2685@gmail.com>
2025-07-28 02:13:00 +0900

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="olygeo",
-    version="0.2.0",
+    version="0.2.1",
     author="Hyeongjoe Chu",
     author_email="you@example.com",
     description="A geometry library for olympiad-style constructions and proofs",
